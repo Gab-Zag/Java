1e0524c6fefe96a144084c13a1019b765fe35fee
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2023-10-03 13:51:05 -0300

    Create Hud.java

---
+++ Projetos/Banco/Hud.java
@@ -0,0 +1,18 @@
+public class Hud {
+    public static void PrimeiroMenu() {
+        System.out.println("======Main=======");
+        System.out.println("1.Novo cliente");
+        System.out.println("2.Ja e cliente");
+        System.out.println("3.Sair do Programa");
+        System.out.println("=================");
+    }
+
+    public static void Segunda_tela_do_banco() {
+        System.out.println("===Menu===");
+        System.out.println("1. Depoisitar");
+        System.out.println("2. Trasferir");
+        System.out.println("3. Extrato");
+        System.out.println("4. Sair para o menu anterior");
+        System.out.println("=================");
+    }
+}
