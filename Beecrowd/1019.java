25fe7655c1d1e792bfa72582b11c648dd0cd6554
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2026-02-11 15:29:12 -0300

    Update 1019.java

--- Beecrowd/1019.java
+++ Beecrowd/1019.java
@@ -1,31 +1,23 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        int x;
-        int seg = 0;
-        int min = 0;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        int m = 0 ;
         int h = 0;
 
-        Scanner len = new Scanner(System.in);
+        n = sc.nextInt();
 
-        x = len.nextInt();
-
-        while (x >= 3600){
-            h++;
-            x = x - 3600;
+        while (n >= 60){
+            n -= 60;
+            m++;
+            if(m >= 60){
+                m = 0;
+                h++;
+            }
         }
-
-        while (x >= 60){
-            min++;
-            x = x - 60;
-        }
-
-        while (x > 0 ){
-            seg++;
-            x--;
-        }
-
-        System.out.println(h + ":" + min + ":" + seg);
+        System.out.println(h + ":" + m + ":" + n);
     }
 }
