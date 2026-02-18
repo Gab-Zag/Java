dd71fda182188dd4b554e739342065e7993273bf
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2026-02-18 03:23:35 -0300

    Create 1059.java

---
+++ Beecrowd/1059.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 1;
+
+        while (n != 101){
+            if((n % 2) == 0){
+                System.out.println(n);
+            }
+            n++;
+        }
+    }
+}
