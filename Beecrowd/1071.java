aa9ea907be888a0520b14bdd9adb3b91560e75b3
Gabriel Alves Pereira <145914573+Gab-Zag@users.noreply.github.com>
2026-02-18 17:35:07 -0300

    Create 1071.java

---
+++ Beecrowd/1071.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int maior = 0;
+        int menor = 0;
+        int soma = 0;
+
+       if (x > y){
+            maior = x;
+            menor = y;
+       }else {
+            menor = x;
+            maior = y;
+       }
+
+        for (int i = menor + 1; i < maior; i++) {
+            if (i % 2 != 0) {
+                soma += i;
+            }
+        }
+
+        System.out.println(soma);
+    }
+}
